e7b01631dafa68d7c12c58b5da895b81d5d9ae08
Yuji ODA <yuji.od@gmail.com>
2014-10-09 21:02:00 +0900

    Fixed typo.

--- app/controllers/gorm.go
+++ app/controllers/gorm.go
@@ -40,7 +40,7 @@ func InitDB() {
 
 		if databaseUrl != "" {
 			re, _ := regexp.Compile("([^:])://([^:]+):([^@]+)@([^/]+)/([^?]+)")
-			match := re.FindSubmatch([]byte(databaseUrl))
+			match := re.FindStringSubmatch([]byte(databaseUrl))
 
 			dbDriver = match[1]
 			if dbDriver == "mysql" {
